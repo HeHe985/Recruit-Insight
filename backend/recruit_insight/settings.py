2faee6ce4653e98054775a038b15dfc0294ec258
gmlfla0219 <dugmlfla0219@gmail.com>
2025-12-18 23:05:35 +0900

    fix: 파일 경로 수정

--- backend/recruit_insight/settings.py
+++ backend/recruit_insight/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,8 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+load_dotenv(BASE_DIR / ".env")
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-oq@60b%x*((ju+6ws_8rn&tx4@@cpw3g(2%6udrkl$e(%11g3)"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
