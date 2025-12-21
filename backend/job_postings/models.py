4203287bee6591789a58f931931a562189b257d5
gmlfla0219 <gmlfla0219@naver.com>
2025-12-22 01:20:49 +0900

    feature: AcrossKnowledgeComparison 모델 정의

--- backend/job_postings/models.py
+++ backend/job_postings/models.py
@@ -156,6 +156,7 @@ class WithinJobAbilityLevelComparison(models.Model):
     job_abl_lvl_cont_cmpr = models.TextField()  # 업무수행능력 수준: 설명
 
 
+# 5
 class AcrossJobAbilityLevelComparison(models.Model):
     job_cd = models.ForeignKey(JobList, on_delete=models.CASCADE)
     job_abl_lvl_status = models.DecimalField()  # 업무수행능력 수준: 중요도(0:낮음 ~ 100:높음)
@@ -163,6 +164,7 @@ class AcrossJobAbilityLevelComparison(models.Model):
     job_abl_lvl_cont = models.TextField()  # 업무수행능력 수준: 설명
 
 
+# 5
 class WithinKnowledgeComparison(models.Model):
     job_cd = models.ForeignKey(JobList, on_delete=models.CASCADE)
     knwldg_status_cmpr = models.DecimalField()  # 지식중요도: 중요도(5점 만점)
@@ -170,8 +172,14 @@ class WithinKnowledgeComparison(models.Model):
     knwldg_cont_cmpr = models.TextField()  # 지식중요도: 설명
 
 
-# class RelatedMajor(models.Model):
-#     job_cd = models.ForeignKey(JobList, on_delete=models.CASCADE)
+# 5
+class AcrossKnowledgeComparison(models.Model):
+    job_cd = models.ForeignKey(JobList, on_delete=models.CASCADE)
+    knwldg_status = models.DecimalField()  # 지식중요도: 중요도(0:낮음 ~ 100:높음)
+    knwldg_nm = models.TextField()  # 지식중요도: 업무수행능력
+    knwldg_cont = models.TextField()  # 지식중요도: 설명
+
+
 # class RelatedMajor(models.Model):
 #     job_cd = models.ForeignKey(JobList, on_delete=models.CASCADE)
 # class RelatedMajor(models.Model):
