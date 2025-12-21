9f7e71779f3c0595cbc8c3d7b2c48ce8601a8899
gmlfla0219 <gmlfla0219@naver.com>
2025-12-22 01:32:09 +0900

    feature: AcrossJobEnvironmentComparison 모델 정의

--- backend/job_postings/models.py
+++ backend/job_postings/models.py
@@ -197,15 +197,21 @@ class AcrossKnowledgeLevelComparison(models.Model):
 
 
 # 5
-class WithinJobsEnvironmentComparison(models.Model):
+class WithinJobEnvironmentComparison(models.Model):
     job_cd = models.ForeignKey(JobList, on_delete=models.CASCADE)
     job_env_status_cmpr = models.DecimalField()  # 업무환경: 중요도(5점 만점)
     job_env_nm_cmpr = models.TextField()  # 업무환경: 업무수행능력
     job_env_cont_cmpr = models.TextField()  # 업무환경: 설명
 
 
-# class RelatedMajor(models.Model):
-#     job_cd = models.ForeignKey(JobList, on_delete=models.CASCADE)
+# 5
+class AcrossJobEnvironmentComparison(models.Model):
+    job_cd = models.ForeignKey(JobList, on_delete=models.CASCADE)
+    job_env_status = models.DecimalField()  # 업무환경: 중요도(0:낮음 ~ 100:높음)
+    job_env_nm = models.TextField()  # 업무환경: 업무수행능력
+    job_env_cont = models.TextField()  # 업무환경: 설명
+
+
 # class RelatedMajor(models.Model):
 #     job_cd = models.ForeignKey(JobList, on_delete=models.CASCADE)
 # class RelatedMajor(models.Model):
