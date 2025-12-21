c15876f3100f36556ef17309673f8ee19811f7e3
gmlfla0219 <gmlfla0219@naver.com>
2025-12-22 01:14:56 +0900

    feature: AcrossJobAbilityLevelComparison 모델 정의

--- backend/job_postings/models.py
+++ backend/job_postings/models.py
@@ -156,8 +156,13 @@ class WithinJobAbilityLevelComparison(models.Model):
     job_abl_lvl_cont_cmpr = models.TextField()  # 업무수행능력 수준: 설명
 
 
-# class RelatedMajor(models.Model):
-#     job_cd = models.ForeignKey(JobList, on_delete=models.CASCADE)
+class AcrossJobAbilityLevelComparison(models.Model):
+    job_cd = models.ForeignKey(JobList, on_delete=models.CASCADE)
+    job_abl_lvl_status = models.DecimalField()  # 업무수행능력 수준: 중요도(0:낮음 ~ 100:높음)
+    job_abl_lvl_nm = models.TextField()  # 업무수행능력 수준: 업무수행능력
+    job_abl_lvl_cont = models.TextField()  # 업무수행능력 수준: 설명
+
+
 # class RelatedMajor(models.Model):
 #     job_cd = models.ForeignKey(JobList, on_delete=models.CASCADE)
 # class RelatedMajor(models.Model):
