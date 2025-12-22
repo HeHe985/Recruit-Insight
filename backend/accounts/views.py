d54ac0489b23c96a231ced4e5d2455eb49bfa58b
gmlfla0219 <dugmlfla0219@gmail.com>
2025-12-23 00:18:01 +0900

    jwt 발급 로직 serializer로 이동

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,19 +1,12 @@
 # Create your views here.
-from django.contrib.auth import authenticate
-from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
+
+from .serializers import LoginSerializer
 
 
 @api_view(["POST"])
 def login(request):
-    username = request.data.get("username")
-    password = request.data.get("password")
-
-    user = authenticate(username=username, password=password)
-    if user is None:
-        return Response({"detail": "invalidcredentials"}, status=status.HTTP_401_UNAUTHORIZED)
-
-    refresh = RefreshToken.for_user(user)
-    return Response({"refresh": str(refresh), "access": str(refresh.access_token)})
+    serializer = LoginSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    return Response(serializer.validated_data)
