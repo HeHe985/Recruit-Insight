ce9e0df1273518b2cb6df04609ef3f228b3526e8
gmlfla0219 <dugmlfla0219@gmail.com>
2025-12-23 23:17:54 +0900

    feature: 메인 페이지 생성

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,12 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>App View</h1>
+  <RouterView />
 </template>
 
-<style scoped></style>
+<script setup>
+  import { RouterView } from 'vue-router';
+</script>
+
+<style scoped>
+
+</style>
