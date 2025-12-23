0683eb2e4e2131a36ba3ec77a424d9571b66db93
gmlfla0219 <dugmlfla0219@gmail.com>
2025-12-23 11:02:00 +0900

    feature: 회원가입 백엔드 기능 구현

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,8 +1,9 @@
 # Create your views here.
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .serializers import LoginSerializer
+from .serializers import LoginSerializer, SignupSerializer
 
 
 @api_view(["POST"])
@@ -10,3 +11,14 @@ def login(request):
     serializer = LoginSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     return Response(serializer.validated_data)
+
+
+@api_view(["POST"])
+def signup(request):
+    serializer = SignupSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(
+        {"message": "회원가입 성공"},
+        status=status.HTTP_201_CREATED,
+    )
