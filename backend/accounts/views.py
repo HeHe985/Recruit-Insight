d0ed54eb4cbf1fac8837006367eb0fabcdb5e9b7
gmlfla0219 <dugmlfla0219@gmail.com>
2025-12-23 12:26:05 +0900

    feature: 로그아웃 백엔드 기능구현

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,6 +1,9 @@
 # Create your views here.
-from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .serializers import LoginSerializer
 
@@ -10,3 +13,20 @@ def login(request):
     serializer = LoginSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     return Response(serializer.validated_data)
+
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def logout(request):
+    refresh = request.data.get("refresh")
+
+    if not refresh:
+        return Response({"detail": "refresh 토큰 필요"}, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        token = RefreshToken(refresh)
+        token.blacklist()
+    except Exception:
+        return Response({"detail": "유효하지 않은 토큰"}, status=status.HTTP_400_BAD_REQUEST)
+
+    return Response({"massage": "로그아웃 완료"}, status=status.HTTP_200_OK)
