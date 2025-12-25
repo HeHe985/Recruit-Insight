3c172a1224184b483267ff84b66ac21b976cdeb1
YangHyeonSeo <ihyeonseo@naver.com>
2025-12-25 13:38:15 +0900

    feature: 자기소개서 상세 페이지 구현

--- frontend/src/views/CoverLetterDetailView.vue
+++ frontend/src/views/CoverLetterDetailView.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>
+  <div v-if="coverLetter">
+    <h3>{{ coverLetter.id }}</h3>
+    <h2>{{ coverLetter.question }}</h2>
+    <p>category : {{ coverLetter.category }}</p>
+    <p>{{ coverLetter.content }}</p>
+    <ul>
+      <li>메모 : {{ coverLetter.note }}</li>
+      <li>최종 수정 일자 : {{ coverLetter.updated_at }}</li>
+      <li>작성 일자 : {{ coverLetter.created_at }}</li>
+    </ul>
 
   </div>
 </template>
 
 <script setup>
+  import axios from 'axios'
+  import { onMounted, ref } from 'vue'
+  import { useRoute } from 'vue-router'
+  import { useCoverLetterStore } from '@/stores/coverletters.js'
 
+  const store = useCoverLetterStore()
+  const route = useRoute()
+  const coverLetter = ref(null)
+
+  onMounted(() => {
+    axios({
+      method: 'get',
+      url: `${store.API_URL}/api/v1/accounts/cover_letters/${route.params.id}`
+    })
+    .then((res) =>{
+      console.log(res.data)
+      coverLetter.value = res.data
+    })
+    .catch(err => console.log(err))
+  })
 </script>
 
 <style scoped>
