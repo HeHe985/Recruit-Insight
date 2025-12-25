e9318ceff5bfe798bdb2136e43d0ebad57abc452
gmlfla0219 <dugmlfla0219@gmail.com>
2025-12-25 19:52:53 +0900

    chore: 액세스 토큰 시간 변경

--- backend/recruit_insight/settings.py
+++ backend/recruit_insight/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
+from datetime import timedelta
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -156,3 +157,11 @@ REST_AUTH = {
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
 ]
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
