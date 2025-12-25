eb95e6dc50dda89f09fc4fe7996fa7c31fbb474d
YangHyeonSeo <ihyeonseo@naver.com>
2025-12-25 14:25:42 +0900

    feature : 자기소개서 작성(C) 기능 구현 중 / 드롭다운 메뉴 이어서 만들기

---
+++ frontend/src/views/CoverLetterCreateView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>자기소개서 작성</h1>
+    <form @submit.prevent="createCoverLetter">
+      <label for="question">자기소개서 문항 : </label>
+      <input type="text" id="question" v-model.trim="question"><br>
+      <label for="category">카테고리</label>
+      <label for="content">답변 : </label>
+      <textarea id="content" v-model.trim="content"></textarea><br>
+      <input type="submit">
+    </form>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  // 드롭다운 메뉴
+  const selectedOption = ref('')
+  const options = ref([
+    { value: '1', text: '지원동기'}
+  ])
+</script>
+
+<style scoped>
+
+</style>
