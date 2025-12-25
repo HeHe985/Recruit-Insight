c49561902e3b402bd4fc1067b4f69848265c69d7
YangHyeonSeo <ihyeonseo@naver.com>
2025-12-25 15:31:51 +0900

    fix: stores/coverletters.js의 API_URL 수정

--- frontend/src/stores/coverletters.js
+++ frontend/src/stores/coverletters.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 export const useCoverLetterStore = defineStore('coverLetter', () => {
   const coverletters = ref([])
-  const API_URL = 'http://127.0.0.1:8000/'
+  const API_URL = 'http://127.0.0.1:8000'
 
   const getCoverLetters = function() {
     axios({
