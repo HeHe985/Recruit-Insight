6974e6a32399e3e043befc4f09737c726e720bc2
YangHyeonSeo <ihyeonseo@naver.com>
2025-12-25 17:50:50 +0900

    feature: 자기소개서 삭제(D) 기능 구현(프론트)

--- frontend/src/views/CoverLetterDetailView.vue
+++ frontend/src/views/CoverLetterDetailView.vue
@@ -10,6 +10,7 @@
       <li>작성 일자 : {{ coverLetter.created_at }}</li>
     </ul>
     <button @click="router.push({ name: 'CoverLetterEditView' , params: { id: route.params.id}})">수정</button>
+    <button @click="deleteCoverLetter">삭제</button>
   </div>
 </template>
 
@@ -27,7 +28,7 @@
   onMounted(() => {
     axios({
       method: 'get',
-      url: `${store.API_URL}/api/v1/accounts/cover_letters/${route.params.id}`
+      url: `${store.API_URL}/api/v1/accounts/cover_letters/${route.params.id}/`
     })
     .then((res) =>{
       console.log(res.data)
@@ -35,6 +36,20 @@
     })
     .catch(err => console.log(err))
   })
+
+  const deleteCoverLetter = function() {
+    if (confirm('정말 삭제하시겠습니까?')) {
+      axios({
+        method: 'delete',
+        url:  `${store.API_URL}/api/v1/accounts/cover_letters/${route.params.id}/`
+      })
+      .then(() => {
+        alert('삭제되었습니다.')
+        router.push({name: 'CoverLetterListView'})
+      })
+      .catch((err) => console.log(err))
+    }
+  }
 </script>
 
 <style scoped>
